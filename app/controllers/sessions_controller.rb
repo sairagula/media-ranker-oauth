11bc537d6eeefbde47a0b2881cd05b0687182947
sairagula <abdukhalieva@gmail.com>
2017-10-16 14:28:31 -0700

    Added routes,also added  methods to controller and model, fields to schema

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,29 +1,57 @@
 class SessionsController < ApplicationController
-  def login_form
-  end
+  skip_before_action :require_login, only: [:create, :auth_callback]
 
-  def login
-    username = params[:username]
-    if username and user = User.find_by(username: username)
-      session[:user_id] = user.id
-      flash[:status] = :success
-      flash[:result_text] = "Successfully logged in as existing user #{user.username}"
-    else
-      user = User.new(username: username)
-      if user.save
+  def create
+    auth_hash = request.env['omniauth.auth']
+
+    if auth_hash['uid']
+      user = User.find_by(uid: auth_hash[:uid], provider: params[:provider])
+      if user.nil?
+        # User has not logged in before
+        # Create a new record in the DB
+        user = User.from_auth_hash(params[:provider],auth_hash)
+        save_and_flash(user)
+
+      else
         session[:user_id] = user.id
         flash[:status] = :success
-        flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
-      else
-        flash.now[:status] = :failure
-        flash.now[:result_text] = "Could not log in"
-        flash.now[:messages] = user.errors.messages
-        render "login_form", status: :bad_request
-        return
+        flash[:message] = "Successfully logged in as returning user #{user.name}"
+
       end
+
+    else
+      flash[:status] = :failure
+      flash[:message] = "Could not create user from OAuth data"
     end
     redirect_to root_path
   end
+
+  # def login_form
+  # end
+  #
+  #
+  # def login
+  #   username = params[:username]
+  #   if username and user = User.find_by(username: username)
+  #     session[:user_id] = user.id
+  #     flash[:status] = :success
+  #     flash[:result_text] = "Successfully logged in as existing user #{user.username}"
+  #   else
+  #     user = User.new(username: username)
+  #     if user.save
+  #       session[:user_id] = user.id
+  #       flash[:status] = :success
+  #       flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
+  #     else
+  #       flash.now[:status] = :failure
+  #       flash.now[:result_text] = "Could not log in"
+  #       flash.now[:messages] = user.errors.messages
+  #       render "login_form", status: :bad_request
+  #       return
+  #     end
+  #   end
+  #   redirect_to root_path
+  # end
 
   def logout
     session[:user_id] = nil
---
+++ db/migrate/20171016202503_add_null_to_user_fields.rb
@@ -0,0 +1,6 @@
+class AddNullToUserFields < ActiveRecord::Migration[5.0]
+  def change
+    change_column :users, :provider, :string, null: false
+    change_column :users, :uid, :string, null: false
+  end
+end
